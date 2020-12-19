5da3f3d7d3d9ecfc3b1a8c8a80c52011dac84d21
jiz148 <jizjiz148148@gmail.com>
2020-12-19 14:47:31 +0800

    modified story

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # built apps,
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
