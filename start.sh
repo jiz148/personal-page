c9fb472ae11e91526ea05d71ed6cb5945a532988
jiz148 <jizjiz148148@gmail.com>
2020-12-31 16:47:05 +0800

    added start.sh and uwsgi.ini

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
- #!/bin/bash
- python manage.py makemigrations&&
- python manage.py migrate&&
- uwsgi --ini /var/www/html/personal-page/uwsgi.ini
- # python manage.py runserver 0.0.0.0:8000
+#!/bin/bash
+python manage.py makemigrations&&
+python manage.py migrate&&
+uwsgi --ini /var/www/html/personal-page/uwsgi.ini
+# python manage.py runserver 0.0.0.0:8000
