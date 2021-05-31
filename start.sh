70c741e3e4aae4772cb8c9575274f229ba5ca111
jiz148 <jizjiz148148@gmail.com>
2021-05-31 18:03:48 +0800

    added docker-compose

--- start.sh
+++ start.sh
@@ -3,7 +3,4 @@ python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
 python manage.py migrate&&
 # python manage.py runserver 0.0.0.0:8000
-uwsgi --ini /var/www/html/personal-page/uwsgi.ini&&
-tail -f /dev/null
-
-exec "$@"
+uwsgi --ini /var/www/html/personal-page/uwsgi.ini
